85aa63814dca3e1c4a5c2e950a6eadab1e8350c6
BT_1N3_12 <BT_1N3_12@BT_1N3_12.bktech.local>
2017-10-19 11:47:24 -0400

    more stuff

--- Amanat Hossain/src/arrays/ObjectArrays.java
+++ Amanat Hossain/src/arrays/ObjectArrays.java
@@ -1,17 +1,20 @@
 package arrays;
 
+import java.util.Arrays;
+
 public class ObjectArrays {
 
 	public ObjectArrays() {
-		Object [] people = new Object[20];
+		Person [] people = new Person[20];
 		populate(people);
-		people[0] = new Thing("coffee maker");
-		for(Object p:people) {
-			System.out.println(p);
+		for(Person p:people) {
+			p.mingle(people);
+			p.printFriends();
+			System.out.println("");
 		}
 	}
 
-	private void populate(Object[] people) {
+	private void populate(Person[] people) {
 		for(int i =0; i<people.length;i++) {
 			String firstName = randomNameFrom(Person.FIRST_START, Person.FIRST_MIDDLE, Person.FIRST_END );
 			String lastName = randomNameFrom(Person.LAST_START, Person.LAST_MIDDLE, Person.LAST_END );
@@ -28,7 +31,7 @@ public class ObjectArrays {
 				people[i] = new Person(firstName, lastName, b);	
 			}
 		}
-		
+
 	}
 
 	private Borough randomBorough() {
@@ -38,9 +41,62 @@ public class ObjectArrays {
 	private String randomNameFrom(String[] a, String[] b, String[] c) {
 		return get(a)+get(b)+get(c);
 	}
-	
+
 	private String get(String[] a) {
 		return a[(int)(Math.random()*a.length)];
 	}
+
+	public Person[] selectGroup(Person[] population, int length) {
+		Person[] group = new Person[length];
+		group[0] = selectAPerson(population);
+		for(int i = 0; i< length;i++) {
+			Person nextPerson = selectAPerson(population);
+			while(personInGroup(group, nextPerson)) {
+				nextPerson = selectAPerson(population);
+			}
+			group[1] = nextPerson;
+		}
+		return group;
+	}
 	
+	/**
+	 * return the number of differences between the two arrays
+	 * A "difference" means they don't have the same element
+	 * at the same position
+	 * PRECONDITION: the two arrays have the same 
+	 * @param arr1
+	 * @param arr2
+	 * @return
+	 */
+	public double countDifferences(Person[] arr1, Person[] arr2) {
+		for(int i =0; i<arr1.length;i++) {
+			if(arr1 == arr2) {
+				
+			}
+		}
+	}
+	
+	/**
+	 * calls count differences on two Person arrays,
+	 * each array must contain the same elements, but randomly ordered
+	 * Print the number of differences between the two arrays
+	 * Do this 100 times, print the average (hint: average needs to be a double)
+	 */
+	public void testShuffling() {
+		
+	}
+
+	public static boolean personInGroup(Person[] group, Person nextPerson) {
+		for(int i = 0; i<group.length; i++) {
+			if(group[i] == nextPerson) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private Person selectAPerson(Person[] population) {
+		int randomNum = (int)(Math.random()*population.length);
+		return population[randomNum];
+	}
 }
