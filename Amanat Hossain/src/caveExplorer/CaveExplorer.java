f4581a4b8f05c47a2960e97b2184cdc75b0bde35
BT_1N3_12 <BT_1N3_12@BT_1N3_12.bktech.local>
2017-10-27 11:46:14 -0400

    cave

--- Amanat Hossain/src/caveExplorer/CaveExplorer.java
+++ Amanat Hossain/src/caveExplorer/CaveExplorer.java
@@ -8,11 +8,29 @@ public class CaveExplorer {
 	public static Scanner in;//for user input
 	public static CaveRoom currentRoom;//changes based on how the user navigated
 	public static Inventory inventory;//where all objects found 
-	
+	public static boolean playing = true;
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		in = new Scanner(System.in);
+				CaveRoom.setUpCaves();
+				inventory = new Inventory();
+				startExploring();
+	}
 
+
+	private static void startExploring() {
+		while(playing) {
+			print(inventory.getDescription());
+			print(currentRoom.getDescription());
+			print("What would you like to do?");
+			String input = in.nextLine();
+			currentRoom.interpretInput(input);
+		}
+	}
+	
+	public static void print(String s) {
+		//NOTE: late,you can replace this line with the more sophisticated "multiLinePrint from Chatbot
+		System.out.println(s);
 	}
 
 }
--- Amanat Hossain/src/caveExplorer/Inventory.java
+++ Amanat Hossain/src/caveExplorer/Inventory.java
@@ -2,4 +2,15 @@ package caveExplorer;
 
 public class Inventory {
 
+	public Inventory() {
+		
+	}
+	public void updateMap() {
+		
+	}
+	
+	public String getDescription() {
+		return "You have nothing in your inventory";
+	}
+
 }
